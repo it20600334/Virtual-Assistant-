7b1defe335ce9707d72d1d6801b5dea3cab80d39
IT20600334 - Eranda Withanage <88669615+it20600334@users.noreply.github.com>
2022-12-04 11:48:24 +0530

    Listener

--- SCR/Main.py
+++ SCR/Main.py
@@ -4,6 +4,11 @@ listener = sr.Recognizer()
 
 try:
     with sr.Microphone() as source:
+        print('Listening.....')
         voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'Alexa' in command:
+            print (command)
 except:
     pass
